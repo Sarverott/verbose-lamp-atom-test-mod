455aa08f093406a7017330a361661788a42da76f
Sett Sarverott <sarverott@gmail.com>
2022-02-16 03:17:23 +0100

    first commit

---
+++ spec/verbose-lamp-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import VerboseLampView from '../lib/verbose-lamp-view';
+
+describe('VerboseLampView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
